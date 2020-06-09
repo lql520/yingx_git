76aa2fe6bef811e93fc961c5d7e5bf07985fbecc
lql <1801109273@qq.com>
2020-06-10 00:27:14 +0800

    操作者A创建了一个实体类和dao

---
+++ src/main/java/com/baizhi/dao/CategoryDao.java
@@ -0,0 +1,7 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Category;
+
+public interface CategoryDao {
+    Category login(String name);
+}
---
+++ src/main/java/com/baizhi/entity/Category.java
@@ -0,0 +1,54 @@
+package com.baizhi.entity;
+
+import java.util.Objects;
+
+public class Category {
+    private String id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) &&
+                Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+}
