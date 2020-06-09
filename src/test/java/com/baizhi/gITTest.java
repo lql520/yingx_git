b02b7b800c310c3b9770faca19f974ec6364b7ef
lql <1801109273@qq.com>
2020-06-09 23:29:13 +0800

    这是新创建了一个测试类

---
+++ src/test/java/com/baizhi/gITTest.java
@@ -0,0 +1,18 @@
+package com.baizhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class gITTest {
+
+    @Test
+    public void contextLoads() {
+
+        System.out.println("======操作者A第一次修改测试类代码=======");
+    }
+
+}
