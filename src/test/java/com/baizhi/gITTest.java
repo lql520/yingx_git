45c3f55443d4aa2d72c168bc169eeab0dbf72a62
lql <1801109273@qq.com>
2020-06-09 23:55:50 +0800

    在dev分支上写了一个方法

--- src/test/java/com/baizhi/gITTest.java
+++ src/test/java/com/baizhi/gITTest.java
@@ -19,7 +19,9 @@ public class gITTest {
         //dev
         int b = 200;
         int c = 300;
-
     }
 
+    public static void main(String[] args) {
+        System.out.println("==-=-=-=-=-=-=");
+    }
 }
